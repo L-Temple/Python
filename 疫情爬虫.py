d487756ce6047186af06818a304b2a829d881f37
zongminggang <1145808079@qq.com>
2020-01-30 21:46:01 +0800

    还需要解决数据比较问题

--- 疫情爬虫.py
+++ 疫情爬虫.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import pymysql
-mainURL = 'https://123.sogou.com/zhuanti/pneumonia.html?fr=sgnews'
+mainURL = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5'
 req = requests.get(url=mainURL)
-req.encoding = 'utf-8'
+#req.encoding = 'utf-8'
 html = req.text
 soup = BeautifulSoup(html, 'lxml')
 db = pymysql.connect(host='127.0.0.1', user='root', password='root', db='pytest', port=3306, charset='utf8')
@@ -12,73 +12,29 @@ cursor = db.cursor()
 zhcn = """alter table content convert to character set utf8;"""
 cursor.execute(zhcn)
 def main():
-    intime(soup)
-    icbar(soup)
-    suspect(soup)
-    cure(soup)
-    dead(soup)
-    #info()
+    soup1 = soup.text.replace('{', '')
+    soup2 = soup1.replace('"', '')
+    soup3 = soup2.replace('\\', '')
+    soup4 = soup3.replace('[', '')
+    soup5 = soup4.replace(']', '')
+    soup6 = soup5.replace('}', '')
+    soup7 = re.search('\d{3,10}',soup6).group() #全国确诊
+    soup8 = soup6.replace(soup7, '')
+    soup9 = re.search('\d{3,10}',soup8).group() #疑似病例
+    soup10 = soup8.replace(soup9, '')
+    soup11 = re.search('\d{3,10}',soup10).group() #死亡人数
+    soup12 = soup10.replace(soup11, '')
+    soup13 = re.search('\d{3,10}',soup12).group() #治愈人数
+    soup14 = re.search('\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}', soup12).group() #时间
+    print('全国确诊'+soup7)
+    print('疑似病例'+soup9)
+    print('死亡人数'+soup11)
+    print('治愈人数'+soup13)
+    print('截至 '+soup14)
     sql = "INSERT INTO content(id,全国确诊,疑似病例,治愈人数,死亡人数,时间) " \
           "VALUES(null,'{}','{}','{}','{}','{}')"
-    sql2 = sql.format(((icbar(soup))), (suspect(soup)), (cure(soup)), (dead(soup)),(intime(soup)))
+    sql2 = sql.format((soup7), (soup9), (soup13), (soup11),('截至 '+soup14))
     cursor.execute(sql2)
     db.commit()
-def icbar(soup):
-    list = soup.find('div', class_='icbar confirm')
-    content = list.find('div',class_='number').text
-    print('全国确诊:'+content)
-    return content
-def suspect(soup):
-    list = soup.find('div', class_='icbar suspect')
-    content = list.find('div',class_='number').text
-    print('疑似病例:'+content)
-    return content
-def cure(soup):
-    list = soup.find('div', class_='icbar cure')
-    content = list.find('div',class_='number').text
-    print('治愈人数:'+content)
-    return content
-def dead(soup):
-    list = soup.find('div', class_='icbar dead')
-    content = list.find('div',class_='number').text
-    print('死亡人数:'+content)
-    return content
-def intime(soup):
-    list = soup.find('p', class_='time-num')
-    content = list.text
-    print(content)
-    return content
-#def info():
-#    list = soup.find_all('p')
-#    content = re.compile(r'[\u4e00-\u9fa5]+')
-#    result = content.findall(list)
-#    print(result)
-def get():
-    sql = 'select * from content order by id desc limit 1'
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    ic = icbar(soup)
-    su = suspect(soup)
-    cu = cure(soup)
-    de = dead(soup)
-    ti = intime(soup)
-    for row in result:
-        icbars = row[0]
-        suspects = row[1]
-        cures = row[2]
-        deads = row[3]
-        intimes = row[4]
-        print(int(ic)-icbars)
-        print(int(su)-suspects)
-        print(int(cu)-cures)
-        print(int(de)-deads)
-        print(intimes)
 if __name__ == '__main__':
-    sql = 'select 时间 from content'
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    if result == intime(soup):
-        main()
-    else:
-        get()
-        main()
+    main()
