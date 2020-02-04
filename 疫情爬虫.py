3859fb958ae18b06c3f1421effc9a5cb8e9f0918
q1145808079 <60178580+q1145808079@users.noreply.github.com>
2020-02-04 19:37:29 +0800

    添加了昨天与前天数据的比较

--- 疫情爬虫.py
+++ 疫情爬虫.py
@@ -27,15 +27,35 @@ def main():
     soup12 = soup10.replace(soup11, '')
     soup13 = re.search('\d{3,10}',soup12).group() #治愈人数
     soup14 = re.search('\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}', soup12).group() #时间
-    print('全国确诊'+soup7)
-    print('疑似病例'+soup9)
-    print('死亡人数'+soup11)
-    print('治愈人数'+soup13)
-    print('截至 '+soup14)
+    print('全国确诊:'+soup7)
+    print('疑似病例:'+soup9)
+    print('死亡人数:'+soup11)
+    print('治愈人数:'+soup13)
+    print('截至:'+soup14)
     sql = "INSERT INTO content(id,全国确诊,疑似病例,治愈人数,死亡人数,时间) " \
           "VALUES(null,'{}','{}','{}','{}','{}')"
     sql2 = sql.format((soup7), (soup9), (soup13), (soup11),('截至 '+soup14))
     cursor.execute(sql2)
     db.commit()
+    t = soup.text
+    last = t[::-1].replace('{', '')
+    last1 =last.replace('"', '')
+    last2 =last1.replace('\\', '')
+    last3 = last2.replace('[', '')
+    last4 = last3.replace(']', '')
+    last5 = last4.replace('}', '')
+    last6 = re.search('\d{2,7}',last5).group() #治愈人数
+    last7 = last5.replace(last6, '')
+    last8 = re.search('\d{2,7}',last7).group() #死亡人数
+    last9 = last7.replace(last8, '')
+    last10 = re.search('\d{2,7}',last9).group() #疑似病例
+    last11 = last9.replace(last10, '')
+    last12 = re.search('\d{2,7}',last11).group() #全国确诊
+    last13 = re.search('\d{2}.\d{2}', last11).group() #时间
+    print('昨天相比前天增加治愈人数:'+last6[::-1])
+    print('昨天相比前天增加死亡人数:'+last8[::-1])
+    print('昨天相比前天增加疑似病例:'+last10[::-1])
+    print('昨天相比前天增加全国确诊:'+last12[::-1])
+    print('昨天日期:'+last13[::-1])
 if __name__ == '__main__':
     main()
