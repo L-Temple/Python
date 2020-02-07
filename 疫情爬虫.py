0a3f665bf73e20a6f89b9465fe19c2e127868e23
q1145808079 <60178580+q1145808079@users.noreply.github.com>
2020-02-07 21:44:19 +0800

    加入了死亡率和治愈率，同时更改了一些内容以适应网站的变化

--- 疫情爬虫.py
+++ 疫情爬虫.py
@@ -26,12 +26,17 @@ def main():
     soup11 = re.search('\d{3,10}',soup10).group() #死亡人数
     soup12 = soup10.replace(soup11, '')
     soup13 = re.search('\d{3,10}',soup12).group() #治愈人数
-    soup14 = re.search('\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}', soup12).group() #时间
+    soup14 = re.search('\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}', soup12).group() #时间\
+    AllPeople = int(soup7)+int(soup9)
+    DeadRate = (int(soup11)/AllPeople)*100
+    HealRate = (int(soup13)/AllPeople)*100
     print('全国确诊:'+soup7)
     print('疑似病例:'+soup9)
     print('死亡人数:'+soup11)
     print('治愈人数:'+soup13)
     print('截至:'+soup14)
+    print("死亡率:%.2f%%" % DeadRate)
+    print("治愈率:%.2f%%" % HealRate)
     sql = "INSERT INTO content(id,全国确诊,疑似病例,治愈人数,死亡人数,时间) " \
           "VALUES(null,'{}','{}','{}','{}','{}')"
     sql2 = sql.format((soup7), (soup9), (soup13), (soup11),('截至 '+soup14))
@@ -44,18 +49,23 @@ def main():
     last3 = last2.replace('[', '')
     last4 = last3.replace(']', '')
     last5 = last4.replace('}', '')
-    last6 = re.search('\d{2,7}',last5).group() #治愈人数
-    last7 = last5.replace(last6, '')
+    l_Date = re.search('\d{2}.\d{2}', last5).group() #时间
+    l_Dates = last5.replace(l_Date, '')
+    l_HealRate = re.search('\d{1,2}.\d{1,2}', l_Dates).group()
+    l_HealsRate = l_Dates.replace(l_HealRate, '')
+    l_DeadRate = re.search('\d{1,2}.\d{1,2}', l_HealsRate).group()
+    l_DeadRates = l_HealsRate.replace(l_DeadRate, '')
+    last6 = re.search('\d{2,7}',l_DeadRates).group() #治愈人数
+    last7 = l_DeadRates.replace(last6, '')
     last8 = re.search('\d{2,7}',last7).group() #死亡人数
     last9 = last7.replace(last8, '')
     last10 = re.search('\d{2,7}',last9).group() #疑似病例
     last11 = last9.replace(last10, '')
     last12 = re.search('\d{2,7}',last11).group() #全国确诊
-    last13 = re.search('\d{2}.\d{2}', last11).group() #时间
     print('昨天相比前天增加治愈人数:'+last6[::-1])
     print('昨天相比前天增加死亡人数:'+last8[::-1])
     print('昨天相比前天增加疑似病例:'+last10[::-1])
     print('昨天相比前天增加全国确诊:'+last12[::-1])
-    print('昨天日期:'+last13[::-1])
+    print('昨天时间:'+l_Date[::-1])
 if __name__ == '__main__':
     main()
